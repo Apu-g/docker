8fe6e7d57d2a6fe5291203856818094d51eb03cd
Apu <apughanti.dev@gmail.com>
2026-05-12 16:48:33 +0530

    yml file and docker compose

---
+++ projects/docker_compose/flask/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, app
+import pymysql
+
+
+
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+@app.route('/insertdata')
+def insert_data():
+    connection = pymysql.connect(
+        host='mysql_container',
+        user='root',
+        password='rootpassword',
+        db='mydatabase',
+        
+    )
+    cursor = connection.cursor()
+    insert_query = "INSERT INTO users (city, temperature) VALUES (%s, %s)"
+    data = ('New York', 25.5)
+
+    cursor.execute(insert_query, data)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return 'Data inserted successfully!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
